5157fdb9878e39d78e79e84fa5edd6618aed5767
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-04-01 13:33:26 +0530

    some fixes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -8,6 +9,7 @@ import adminRoutes from "./routes/adminRoutes.js";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
